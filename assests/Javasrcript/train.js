d0091e8c215030cef2fb5e31e2119bf2759480ac
Michael Kovacic <michael.j.kovacic@gmail.com>
2018-04-11 11:38:59 -0600

    cleaned code, enabed update function

--- assests/Javasrcript/train.js
+++ assests/Javasrcript/train.js
@@ -1,6 +1,6 @@
 
 // Initialize Firebase
-var config = {
+const config = {
     apiKey: "AIzaSyAiNJmUWd5FaT84Chs__rJ5Nx7Z-HLWNdA",
     authDomain: "trainschedule-7ac28.firebaseapp.com",
     databaseURL: "https://trainschedule-7ac28.firebaseio.com",
@@ -9,16 +9,23 @@ var config = {
     messagingSenderId: "492771587094"
 };
 firebase.initializeApp(config);
+const database = firebase.database();
+//set the variables
+let trainName = "";
+let trainTime;
+let destination = "";
+let frequency;
+let nextArrival = 0;
+let minutesAway = 0;
 
-var database = firebase.database();
 
+//this enables the current time and train arrival time to update
+$(document).ready(function(){
+    datetime = $('#datetime')
+    update();
+    setInterval(update, 1000);
 
-var trainName = "";
-var trainTime;
-var destination = "";
-var frequency = "";
-var nextArrival = 0;
-var minutesAway = 0;
+});
 
 //click funtion to gather data and input into firebase
 $("#submit").on("click", function(event){
@@ -26,8 +33,7 @@ $("#submit").on("click", function(event){
 
   trainName = $("#train-name").val().trim();
   destination = $("#destination").val().trim();
-
-  trainTime = moment($("#start-time").val().trim(), "hh:mm").format("X");
+  trainTime = moment($("#start-time").val().trim(), "HH:mm").format("HH:mm");
   frequency = $("#frequency").val().trim();
 
   database.ref().push({
@@ -42,130 +48,90 @@ $("#submit").on("click", function(event){
 //function to call the data from firebase and input into html
 database.ref().on("child_added", function(childSnapshot){
 
-  var frequency = childSnapshot.val().frequency;
-  var trainTime = childSnapshot.val().trainTime;
-  
+  let frequency = childSnapshot.val().frequency;
+  let trainTime = childSnapshot.val().trainTime;
+    //logic to determine when the next train is going to arrive
+    let trainTimeConverted = moment(trainTime, "HH:mm").subtract(1,"years");
+    let currentTime = moment();
+    let diffTime = moment().diff(moment(trainTimeConverted), "minutes");
+    let tRemainder = diffTime % frequency;
+    let minutesAway = frequency - tRemainder;
+    let nextArrival = moment().add(minutesAway, "minutes").format("hh:mma");
+    //generates the data into the HTML
+    const generateRow = $("<tr>");
 
-  
-  var trainTimeConverted = moment(trainTime, "hh:mm").subtract(1,"years");
-  //console.log(trainTimeConverted);
+    generateRow.html("<td>" + childSnapshot.val().trainName + "</td>" + 
+      "<td>" + childSnapshot.val().destination + "</td>" + 
+      "<td>" + childSnapshot.val().frequency + "</td>" + 
+      "<td id=nextArrival>" + nextArrival + "</td>" + 
+      "<td id=minutesAway>" + minutesAway + "</td>");
 
-  var currentTime = moment();
-
-  var diffTime = moment().diff(moment(trainTimeConverted), "minutes");
-
-  var tRemainder = diffTime % frequency;
-
-  var minutesAway = frequency - tRemainder;
-
-  var nextArrival = moment().add(minutesAway, "minutes").format("hh:mma");
-
-
-
-  var generateRow = $("<tr>");
-
-  generateRow.html("<td>" + childSnapshot.val().trainName + "</td>" + 
-    "<td>" + childSnapshot.val().destination + "</td>" + 
-    "<td>" + childSnapshot.val().frequency + "</td>" + 
-    "<td id=nextArrival>" + nextArrival + "</td>" + 
-    "<td id=minutesAway>" + minutesAway + "</td>");
-
-  $(".train-sect").append(generateRow);
-
-  // console.log(childSnapshot.val().trainName);
-  // console.log(childSnapshot.val().destination);
-  // console.log(childSnapshot.val().frequency);
-
+    $(".train-sect").append(generateRow);
   //this clears the values in the form
   $("#train-name").val("");
   $("#start-time").val("");
   $("#destination").val("");
   $("#frequency").val("");
-
-  update(frequency, trainTime)
-
 }, function(errorObject) {
       console.log("Errors handled: " + errorObject.code);
 });
 
-//orders the given data from firebas into sequential order by its added date
-database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
-  var generateRow = $("<tr>");
-  generateRow.html("<td>" + snapshot.val().trainName + "</td>" + 
-    "<td>" + snapshot.val().destination + "</td>" + 
-    "<td>" + snapshot.val().frequency + "</td>" + 
-    "<td>" + nextArrival + "</td>" + 
-    "<td>" + minutesAway + "</td>");
+//orders the given data from firebase into sequential order by its added date
+// database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
+//   var generateRow = $("<tr>");
+//   generateRow.html("<td>" + snapshot.val().trainName + "</td>" + 
+//     "<td>" + snapshot.val().destination + "</td>" + 
+//     "<td>" + snapshot.val().frequency + "</td>" + 
+//     "<td>" + nextArrival + "</td>" + 
+//     "<td>" + minutesAway + "</td>");
   
-  $("#add-employee").append(generateRow);
-});
+//   $("#add-employee").append(generateRow);
+// });
 
+let datetime = null;
+let date = null;
 
-// var momentOne = moment().format("h:mm:ss");
-// console.log(momentOne);
-
-var datetime = null,
-        date = null;
-
-var update = function (frequency, trainTime) {
-    date = moment(new Date()).format("h:mm:ss")
-    $("#currentTime").text(date)
-
-    var refresh = moment().format("ss");
+//runs the clock and updates the arrival time infor every minute
+function update() {
+  date = moment(new Date()).format("h:mm:ss")
+  $("#currentTime").text(date)
+  let refresh = moment().format("ss");
    
-   if(refresh == 00){
-      location.reload();
-      // var frequency = frequency;
-      // var trainTime = trainTime;
-      
+  if(refresh == 00){
+    $(".train-sect").empty()
+      database.ref().on("child_added", function(childSnapshot){
 
-      
-      // var trainTimeConverted = moment(trainTime, "hh:mm").subtract(1,"years");
-      // //console.log(trainTimeConverted);
+        let frequency = childSnapshot.val().frequency;
+        let trainTime = childSnapshot.val().trainTime;
+        
+          let trainTimeConverted = moment(trainTime, "HH:mm").subtract(1,"years");
+          let currentTime = moment();
+          let diffTime = moment().diff(moment(trainTimeConverted), "minutes");
+          let tRemainder = diffTime % frequency;
+          let minutesAway = frequency - tRemainder;
+          let nextArrival = moment().add(minutesAway, "minutes").format("hh:mma");
+          //generates the data into the HTML
+            const generateRow = $("<tr>");
 
-      // var currentTime = moment();
+            generateRow.html("<td>" + childSnapshot.val().trainName + "</td>" + 
+              "<td>" + childSnapshot.val().destination + "</td>" + 
+              "<td>" + childSnapshot.val().frequency + "</td>" + 
+              "<td id=nextArrival>" + nextArrival + "</td>" + 
+              "<td id=minutesAway>" + minutesAway + "</td>");
 
-      // var diffTime = moment().diff(moment(trainTimeConverted), "minutes");
+            $(".train-sect").append(generateRow);
 
-      // var tRemainder = diffTime % frequency;
+        //this clears the values in the form
+        $("#train-name").val("");
+        $("#start-time").val("");
+        $("#destination").val("");
+        $("#frequency").val("");
 
-      // var minutesAway = frequency - tRemainder;
-
-      // var nextArrival = moment().add(minutesAway, "minutes").format("hh:mma");
-
-      // $("#nextArrival").text(nextArrival);
-      // $("#minutesAway").text(minutesAway);
-
-
-   }
-    
+      }, function(errorObject) {
+            console.log("Errors handled: " + errorObject.code);
+      });
+   };
 };
 
-$(document).ready(function(){
-    datetime = $('#datetime')
-    update();
-    setInterval(update, 1000);
 
 
-
-      // $.ajax({
-      //   url: "https://trainschedule-7ac28.firebaseio.com/",
-      //   method: "GET"
-      //   }).then(function(results){
-      //     console.log("results: " + results);
-      //     console.log(status);
-      // })
-
-
-});
-
-
-
-
-//for all the info submitted i want to commit that info to firsebase 
-
-//next i need to pull that info from firebase and input it into the html
-
-//with the submitted in i need to create a formula to input the remainder fields
-
-//
